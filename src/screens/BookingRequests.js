638d28e68bec965dd1e1814a7939767ee9932b6f
Garvik <apple@Apples-MacBook-Pro.local>
2024-04-19 01:45:26 +0530

    one booking at a time

--- src/screens/BookingRequests.js
+++ src/screens/BookingRequests.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { showToast } from '../helpers/ShowToast';
 import { useNavigation } from '@react-navigation/native';
 import RouteNames from '../routes/RouteNames';
-import Loader from '../components/Loader';
 import { getAllBookingRequests } from '../actions/BookingRequestsAction';
 import BookingRequestsCard from '../components/BookingRequests/BookingRequestsCard';
 import { FlatList } from 'react-native';
 import { CLEAR_ERRORS } from '../constants/BookingRequestsConstants';
 import Colors from '../helpers/Colors';
+import {
+  GET_CURRENT_BOOKINGS_FAIL,
+  GET_CURRENT_BOOKINGS_REQUEST,
+  GET_CURRENT_BOOKINGS_SUCCESS,
+} from '../constants/BookingsConstants';
+import axiosInstance, { BASE_URL } from '../Axios';
 
 const BookingRequests = () => {
   const dispatch = useDispatch();
@@ -18,9 +23,14 @@ const BookingRequests = () => {
   const { loading, bookingRequests } = useSelector(
     state => state.bookingRequests,
   );
+  const { currentBookings } = useSelector(
+    state => state.currentBookings,
+  );
   const { error, isUpdated } = useSelector(state => state.bookingRequest);
 
   useEffect(() => {
+    dispatch(getCurrentBookings());
+
     const getBookings = () => {
       dispatch(getAllBookingRequests());
     }
@@ -43,19 +53,48 @@ const BookingRequests = () => {
     }
   }, [isAuthenticated, error, isUpdated]);
 
+  // get all current bookings
+  const getCurrentBookings = () => async dispatch => {
+    try {
+      dispatch({ type: GET_CURRENT_BOOKINGS_REQUEST });
+      const { data } = await axiosInstance.get(
+        `${BASE_URL}/api/v1/bookings/current`,
+      );
+
+      dispatch({ type: GET_CURRENT_BOOKINGS_SUCCESS, payload: data.bookings });
+    } catch (error) {
+      dispatch({
+        type: GET_CURRENT_BOOKINGS_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+
   // return loading ? (
   //   <Loader />
   // ) : (
-  return <View>
-    <FlatList
-      ListEmptyComponent={() => (
-        <Text style={styles.emptylistText}>No new booking requests</Text>
-      )}
-      data={bookingRequests}
-      renderItem={({ item, index }) => index == 0 && <BookingRequestsCard bookingRequest={item} />}
-      contentContainerStyle={{ paddingBottom: 13 }}
-      showsVerticalScrollIndicator={false}
-    />
+  return <View style={{ padding: 20 }}>
+    {
+      currentBookings.length < 1 ?
+        <FlatList
+          ListEmptyComponent={() => (
+            <Text style={styles.emptylistText}>No new booking requests</Text>
+          )}
+          data={bookingRequests}
+          renderItem={({ item, index }) => index == 0 && <BookingRequestsCard bookingRequest={item} />}
+          contentContainerStyle={{ paddingBottom: 13 }}
+          showsVerticalScrollIndicator={false}
+        />
+        :
+        <View >
+          <Text style={styles.emptylistText}>
+            Booking Already in queue. Complete that to get new bookings
+          </Text>
+          <Button title="Show my booking" onPress={() => {
+            navigation.navigate(RouteNames.TODAY_BOOKINGS)
+          }} />
+        </View>
+    }
   </View>
   // );
 };
@@ -63,8 +102,8 @@ const BookingRequests = () => {
 const styles = StyleSheet.create({
   emptylistText: {
     textAlign: 'center',
-    fontSize: 25,
-    color: Colors.RED,
+    fontSize: 22,
+    color: Colors.GRAY,
     fontWeight: 'bold',
     marginVertical: 16,
   },
